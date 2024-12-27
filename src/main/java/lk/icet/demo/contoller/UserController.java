f90be638e40cf42a5d9275bc889bd44f80768c23
Lahiru <wlavishka.95@gmail.com>
2024-12-27 13:20:08 +0530

    first commit

---
+++ src/main/java/lk/icet/demo/contoller/UserController.java
@@ -0,0 +1,21 @@
+package lk.icet.demo.contoller;
+
+
+import lk.icet.demo.dto.User;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/auth/login")
+    public boolean login(@RequestBody User user){
+        return false;
+    }
+   @PostMapping("/register")
+    public String register(@RequestBody User user){
+        return "register user";
+    }
+}
